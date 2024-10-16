14ff66c8535bef6a4c81b24a38d5ab5d33200fb7
SJFERRAZ <s.ferraz@live.com>
2024-10-16 17:14:09 -0300

    feat: adicionando rota de produtos

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.get("/", (req, res) =>{
     res.json({
-        message: "Olá"
+        message: "Olá Zé"
     });
+});
+
+app.get('/produtos', (req, res) => {
+    res.json([{nome: 'celular'}, {nome: 'tv'}, {nome: 'sofá'}, {nome: 'video-game'}, {nome: 'notebook'}]);
 });
 
 const PORT = 3000;
